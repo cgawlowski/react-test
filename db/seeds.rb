0c55ceb27da300f76f9f4fd162ff4a9d208c569a
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2022-07-22 18:40:24 -0400

    Added Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create(
+  name: "Festival international de Jazz de Montreal",
+  description: "Le Festival international de jazz de Montréal est un festival mettant en vedette des artistes et musiciens surtout issus du jazz, qui se déroule annuellement au mois de juillet à Montréal depuis 19801. Selon l'édition 2004 du Guinness World Records, il est l'un des plus importants festivals de jazz au monde en termes d'affluence.",
+  start_date: Time.now,
+  end_date: Time.now + 60
+)
+
+Event.create(
+  name: "Festival MURAL",
+  description: "Le Festival MURAL est un festival international annuel d'art urbain.
+  Il a lieu en juin à Montréal, Québec, Canada. Inauguré en 2013, il vise à célébrer la démocratisation de l'art de rue à Montréal.",
+  start_date: Time.now,
+  end_date: Time.now + 60
+)
+
+Event.create(
+  name: "Juste pour Rire",
+  description: "Plus grand festival du genre, il invite des humoristes du monde entier.
+  Il propose des spectacles d'humour en salles, des théâtres de rue et des parades, des projections de films humoristiques, des émissions de télévision et des tournées en région.",
+  start_date: Time.now,
+  end_date: Time.now + 60
+)
