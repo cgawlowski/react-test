3bbdba557de59266a646e1078c4eb86ab97874d8
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2022-07-22 19:02:44 -0400

    Added CRUD actions in controllers

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,2 +1,66 @@
 class Api::V1::EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
+  # GET /events
+  # GET /events.json
+  def index
+    @events = Event.all.order(brand: :asc)
+    render json: @events
+  end
+
+  # GET /events/1
+  # GET /events/1.json
+  def show
+    if @event
+      render json: @event
+    else
+      render json: @event.errors
+    end
+  end
+
+  # GET /events/new
+  def new
+    @event = Event.new
+  end
+
+  # GET /events/1/edit
+  def edit
+  end
+
+  # POST /events
+  # POST /events.json
+  def create
+    @event = Event.new(event_params)
+
+
+    if @event.save
+      render json: @event
+    else
+      render json: @event.errors
+    end
+  end
+
+  # PATCH/PUT /events/1
+  # PATCH/PUT /events/1.json
+  def update
+  end
+
+  # DELETE /events/1
+  # DELETE /events/1.json
+  def destroy
+    @event.destroy
+
+    render json: { notice: 'event was successfully removed.' }
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def event_params
+      params.permit(:name, :description, :start_date, :end_date)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'events/index'
+      post 'events/create'
+      delete 'events/:id', to: 'events#destroy'
+    end
+  end
+
   root 'events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
